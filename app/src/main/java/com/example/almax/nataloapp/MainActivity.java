fe4edd97e54940e8f217c218d2a7981377031686
almax85 <almax85@hotmail.it>
2017-11-11 16:15:02 +0100

    Upload 2

--- app/src/main/java/com/example/almax/nataloapp/MainActivity.java
+++ app/src/main/java/com/example/almax/nataloapp/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ToggleButton;
 import android.widget.Switch;
 import android.support.v7.app.ActionBar;
